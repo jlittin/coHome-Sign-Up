17656340963041a94d8ee7267d687a9bf22ee1e8
jlittin <josh@huffle.com.au>
2017-04-27 15:29:29 +1000

    Testing

--- js/script.js
+++ js/script.js
@@ -158,3 +158,69 @@ $(document).ready(function () {
 
     $(".lets-do-it").click(page5_next);
   });
+
+
+  var stripe = Stripe('pk_test_LZ23LLdE0qaAyqDHb4OjX98s');
+var elements = stripe.elements();
+// Custom styling can be passed to options when creating an Element.
+var style = {
+  base: {
+    // Add your base input styles here. For example:
+    fontSize: '20px',
+    lineHeight: '40px'
+  }
+};
+
+// Create an instance of the card Element
+var card = elements.create('card', {style: style});
+
+// Add an instance of the card Element into the `card-element` <div>
+card.mount('#card-element');
+
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+// Create a token or display an error when the form is submitted.
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+
+function stripeTokenHandler(token) {
+  // Insert the token ID into the form so it gets submitted to the server
+  var form = document.getElementById('payment-form');
+  var hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', 'stripeToken');
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+
+  // Submit the form
+  form.submit();
+}
+
+// This will work:
+document.getElementById("button").addEventListener("click", function() {
+  handler.open({
+    image: '/square-image.png',
+    name: 'Demo Site',
+    description: '2 widgets',
+    amount: 2000
+  });
+});
